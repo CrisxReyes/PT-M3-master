c540f34c6351ce6dc42ca04282c27225fc79ba2d
Cristopher Reyes <critopher.smnyl@gmail.com>
2022-06-01 21:58:00 -0500

    hasta ls

--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,19 @@
-
+var fs = require("fs");
 
 module.exports = {
-
+    date: () => {
+        process.stdout.write(Date());
+    },
+    pwd: () => {
+        process.stdout.write('Directorio actual: '+ process.cwd());
+    }, 
+    ls: () => {
+        fs.readdir('.', function(err, files) {
+            if (err) throw err;
+            files.forEach(function(file) {
+              process.stdout.write(file.toString() + "\n");
+            })
+            process.stdout.write("prompt > ");
+          });
+    }
 }
