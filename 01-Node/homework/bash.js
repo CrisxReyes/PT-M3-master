175c81afbbb3831e2a2d66a9b93d74ed71cc6a4d
Cristopher Alberto Reyes Cruz <Cristopher.reyes@amher.com.mx>
2022-06-03 16:08:53 -0500

    echo en proceso

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -15,15 +15,18 @@ const commands = require('./commands');
 process.stdout.write('prompt > ');
 // El evento stdin 'data' se dispara cuando el user escribe una línea
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remueve la nueva línea
-  if(cmd === 'date') {
-    commands[cmd]();  
+  var [cmd, ...args] = data.toString().trim().split(' '); // remueve la nueva línea
+
+/*   if (cmd === 'date') {
+    commands[cmd]();
   }
-  if(cmd === 'pwd') {
-    commands[cmd](); 
+  if (cmd === 'pwd') {
+    commands[cmd]();
   }
-  if(cmd === 'ls') {
-    commands[cmd]();  
-  }
+  if (cmd === 'ls') {
+    commands[cmd]();
+  } */
+  commands.hasOwnProperty(cmd) && commands[cmd]();
+
   process.stdout.write('\nprompt > ');
 });
