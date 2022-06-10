788568f587adf4fc5f5660985c77e028cb1de6a8
Cristopher Alberto Reyes Cruz <Cristopher.reyes@amher.com.mx>
2022-06-10 17:43:26 -0500

    Primer img

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,13 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer( function(req, res){ // Creamos una serie de events listener, que van a escuchar por requests que ocurren en este socket
+ //Para crear un response empezamos escribiendo el header
+    if (req.url === '/images/arcoiris_doge.jpg') {
+        res.writeHead(200, { 'Content-Type': 'image/jpeg' }) //Le ponemos el status code y algunos pair-values en el header
+        var img = fs.readFileSync(__dirname + '/images/arcoiris_doge.jpg')
+        res.end(img);
+
+    }
+
+}).listen(1337, '127.0.0.1'); 
