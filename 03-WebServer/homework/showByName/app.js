6cbe531cba7b95d5d4c5d8ec417d2cd10c36ba98
Cristopher Alberto Reyes Cruz <Cristopher.reyes@amher.com.mx>
2022-06-13 13:23:42 -0500

    showByName completa

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -4,15 +4,15 @@ var http  = require("http")
 // Escribí acá tu servidor
 http.createServer( function(req, res){ // Creamos una serie de events listener, que van a escuchar por requests que ocurren en este socket
  //Para crear un response empezamos escribiendo el header
-    /* if (req.url === '/images/arcoiris_doge.jpg') {
-        res.writeHead(200, { 'Content-Type': 'image/jpeg' }) 
-        var img = fs.readFileSync(__dirname + '/images/arcoiris_doge.jpg')
-        res.end(img);
-    } */
-    var files = fs.readdirSync(__dirname + '/images')
+    var files = fs.readdirSync(__dirname + '/images');
+    var filename = req.url.split('/images/')
 
-    if (req.url === '/images/arcoiris_doge.jpg') {
+    if (files.includes(filename[1])) {
         res.writeHead(200, { 'Content-Type': 'image/jpeg' }) 
+        var img = fs.readFileSync(__dirname + req.url)
         res.end(img);
-
+    }else{
+        res.writeHead(404, { 'Content-Type': 'text/plain' }) 
+        res.end('404 NOT-FOUND');
+    }       
 }).listen(1337, '127.0.0.1'); 
